73d892d4b1db4c6819cff61ddc200c43a9875af2
UdayBari3421 <115573575+UdayBari3421@users.noreply.github.com>
2023-10-25 09:12:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,70 +1,77 @@
-const player1Input = document.getElementById("player1");
-const player2Input = document.getElementById("player2");
-const submitButton = document.getElementById("submit");
-const messageDiv = document.querySelector(".message");
-const cells = document.querySelectorAll(".cell");
+document.addEventListener('DOMContentLoaded', function () {
+            const container = document.querySelector('.container');
+            const submitButton = document.getElementById('submit');
+            const board = document.getElementById('board');
+            const message = document.querySelector('.message');
+            let currentPlayer = 1;
+            let player1Name, player2Name;
 
-let currentPlayer = "X";
+            submitButton.addEventListener('click', function () {
+                player1Name = document.getElementById('player-1').value;
+                player2Name = document.getElementById('player-2').value;
 
-submitButton.addEventListener("click", startGame);
+                container.removeChild(submitButton);
+                container.removeChild(document.getElementById('player-1'));
+                container.removeChild(document.getElementById('player-2'));
 
-function startGame() {
-    const player1Name = player1Input.value;
-    const player2Name = player2Input.value;
+                const h1 = document.createElement('h1');
+                h1.textContent = 'Tic Tac Toe';
+                container.appendChild(h1);
 
-    if (!player1Name || !player2Name) {
-        alert("Please enter names for both players.");
-        return;
-    }
+                container.appendChild(message);
 
-    player1Input.disabled = true;
-    player2Input.disabled = true;
-    submitButton.disabled = true;
+                for (let i = 1; i <= 9; i++) {
+                    const cell = document.createElement('div');
+                    cell.classList.add('cell');
+                    cell.id = i;
+                    cell.addEventListener('click', handleCellClick);
+                    board.appendChild(cell);
+                }
 
-    messageDiv.textContent = `${player1Name}, you're up!`;
+                updateMessage();
+            });
 
-    cells.forEach(cell => {
-        cell.addEventListener("click", () => makeMove(cell, player1Name, player2Name));
-    });
-}
+            function handleCellClick() {
+                const cell = event.target;
 
-function makeMove(cell, player1Name, player2Name) {
-    if (cell.textContent === "") {
-        cell.textContent = currentPlayer;
-        cell.style.pointerEvents = "none";
+                if (!cell.textContent) {
+                    cell.textContent = currentPlayer === 1 ? 'X' : 'O';
+                    if (checkWin()) {
+                        message.textContent = `${currentPlayer === 1 ? player1Name : player2Name}, congratulations you won!`;
+                        disableClicks();
+                    } else if (checkDraw()) {
+                        message.textContent = 'It\'s a draw!';
+                    } else {
+                        currentPlayer = 3 - currentPlayer; // Switch player
+                        updateMessage();
+                    }
+                }
+            }
 
-        if (checkWin()) {
-            messageDiv.textContent = `${currentPlayer === "X" ? player1Name : player2Name} congratulations, you won!`;
-            cells.forEach(cell => (cell.style.pointerEvents = "none"));
-        } else {
-            currentPlayer = currentPlayer === "X" ? "O" : "X";
-            messageDiv.textContent = `${currentPlayer === "X" ? player1Name : player2Name}, you're up!`;
-        }
-    }
-}
+            function updateMessage() {
+                message.textContent = `${currentPlayer === 1 ? player1Name : player2Name}, you're up!`;
+            }
 
-function checkWin() {
-    const winningCombos = [
-        [1, 2, 3],
-        [4, 5, 6],
-        [7, 8, 9],
-        [1, 4, 7],
-        [2, 5, 8],
-        [3, 6, 9],
-        [1, 5, 9],
-        [3, 5, 7]
-    ];
+            function checkWin() {
+                const cells = document.querySelectorAll('.cell');
+                const winningCombos = [
+                    [1, 2, 3], [4, 5, 6], [7, 8, 9],
+                    [1, 4, 7], [2, 5, 8], [3, 6, 9],
+                    [1, 5, 9], [3, 5, 7]
+                ];
 
-    for (const combo of winningCombos) {
-        const [a, b, c] = combo;
-        if (
-            cells[a - 1].textContent === cells[b - 1].textContent &&
-            cells[b - 1].textContent === cells[c - 1].textContent &&
-            cells[a - 1].textContent !== ""
-        ) {
-            return true;
-        }
-    }
+                return winningCombos.some(combo =>
+                    combo.every(cellNum => cells[cellNum - 1].textContent === (currentPlayer === 1 ? 'X' : 'O'))
+                );
+            }
 
-    return false;
-}
+            function checkDraw() {
+                const cells = document.querySelectorAll('.cell');
+                return Array.from(cells).every(cell => cell.textContent !== '');
+            }
+
+            function disableClicks() {
+                const cells = document.querySelectorAll('.cell');
+                cells.forEach(cell => cell.removeEventListener('click', handleCellClick));
+            }
+        });
